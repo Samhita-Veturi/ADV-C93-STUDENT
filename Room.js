47617f2c5f871c60a779f88be12e4da597a9ab35
Samhita-Veturi <67774716+Samhita-Veturi@users.noreply.github.com>
2021-01-22 23:30:45 +0530

    Add files via upload

--- Room.js
+++ Room.js
@@ -1,28 +1,46 @@
-function Log_Out(){
-    window.location = "Login.html";
-}
-
-var UserName_1 = localStorage.getItem("User_1");
-
-document.getElementById("UserName_Display").innerHTML = "Welcome " + UserName_1;
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyBJBDhoV-eBulRyZ552vayxsSF0ouwvpSA",
-    authDomain: "c94-project-student.firebaseapp.com",
-    databaseURL: "https://c94-project-student-default-rtdb.firebaseio.com",
-    projectId: "c94-project-student",
-    storageBucket: "c94-project-student.appspot.com",
-    messagingSenderId: "646954476168",
-    appId: "1:646954476168:web:7b6cd60770caff1890f53d"
-  };
+  apiKey: "AIzaSyBgE6YBmHl3TmnzJPV44OjKkMTLd9A1ttE",
+  authDomain: "c-95-project.firebaseapp.com",
+  databaseURL: "https://c-95-project-default-rtdb.firebaseio.com",
+  projectId: "c-95-project",
+  storageBucket: "c-95-project.appspot.com",
+  messagingSenderId: "512265434057",
+  appId: "1:512265434057:web:3e4ec92612fdd78a6eee18"
+};
 // Initialize Firebase
 
+var UserName_1 = localStorage.getItem("User_1");
+document.getElementById("UserName_Display").innerHTML = "Welcome " + UserName_1;
+function rediectToRoomName(Names){
+  console.log(Names);
+  localStorage.setItem("Room_Name", Names);
+  window.location = "Page.html";
+}
+
 firebase.initializeApp(firebaseConfig);
+  function getData() {firebase.database().ref("/").on('value',function(snapshot) {document.getElementById("Output").innerHTML ="";snapshot.forEach(function(childSnapshot) {childKey =
+      childSnapshot.key;
+      Room_Names = childKey;
+      //Start code
+      console.log("Room Names: " + Room_Names);
+      Row = "<div class='room_name' id="+Room_Names+" onclick='redirectToRoomName(this.id)'>#"+ Room_Names +"</div><hr>";
+      document.getElementById("Output").innerHTML+= Row;
+      //End code
+   });});
+}
+  getData();
+
 function AddRoom(){
     Add_User = document.getElementById("Input_Room_Name").value;
     console.log(Add_User);
     firebase.database().ref("/").child(Add_User).update({
-        Purpose: "Comment"
+        Purpose: "Adding Room"
     });
-    document.getElementById("Room_Names").innerHTML = Add_User;
+    document.getElementById("Output").innerHTML += Add_User;
+}
+function LogOut(){
+  localStorage.removeItem("User_1");
+  localStorage.removeItem("Room_Name");
+  window.location = "Login.html";
 }
